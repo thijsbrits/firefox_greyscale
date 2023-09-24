8220ce74f6d4a5a9d4dc4cc30f7dc5b23155b89f
thijsbrits <thijsbrits@gmail.com>
2023-09-24 11:57:12 +0200

    url storage

--- content.js
+++ content.js
@@ -1,10 +1,27 @@
 document.documentElement.style.filter = 'grayscale(100%)';
-
 let isGreyscale = true;
 
+const url = window.location.hostname;
+
+// Fetch and apply stored preference
+browser.storage.local.get(url, function(data) {
+  if (data[url] !== undefined) {
+    isGreyscale = data[url];
+  }
+  applyGreyscale();
+});
+
+function applyGreyscale() {
+  document.documentElement.style.filter = isGreyscale ? 'grayscale(100%)' : '';
+}
+
+// Function to toggle and store greyscale
 function toggleGreyscale() {
   isGreyscale = !isGreyscale;
-  document.documentElement.style.filter = isGreyscale ? "grayscale(100%)" : "";
+  applyGreyscale();
+  const saveObj = {};
+  saveObj[url] = isGreyscale;
+  browser.storage.local.set(saveObj);
 }
 
 // Listen for messages from the popup
